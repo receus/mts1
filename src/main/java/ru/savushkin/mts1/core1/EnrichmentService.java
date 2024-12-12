f6456dbc267b584aa548a39fdabefcf96cc7470a
Ramazanov Stanislav <mhalkertu@gmail.com>
2024-12-12 22:35:37 +0700

    исправил код

--- src/main/java/ru/savushkin/mts1/core1/EnrichmentService.java
+++ src/main/java/ru/savushkin/mts1/core1/EnrichmentService.java
@@ -5,10 +5,21 @@ import java.util.Collections;
 import java.util.List;
 
 public class EnrichmentService {
-
+    private final EnrichmentProcessor processor;
+    private final MessageValidator validator;
 
     public String enrich(Message message) {
 
+        if(!validator.validate(message)) {
+            return message.getContent();
+        }
+        if(message.getEnrichmentType() == Message.EnrichmentType.MSISDN){
+            return processor.enrich(message.getContent());
+        }
         return message.getContent();
+    }
+    public EnrichmentService(EnrichmentProcessor processor, MessageValidator validator) {
+        this.processor = processor;
+        this.validator = validator;
     }
 }
